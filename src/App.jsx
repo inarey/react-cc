368d3bda2201c2fc2a3ebfefb33cdfd0e4ba7cb2
shan-ti <thoselovelywords@gmail.com>
2023-02-16 10:42:52 +0900

    updated the card styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,54 @@ function App() {
   const showEmployee = true
 
   return (
-    <div className="bg-teal-500">
+    <div className='bg-teal-500'>
       {showEmployee ? (
         <>
-          <input type="text" onChange={(e) => {
-            console.log(e.target.value);
-            setRole(e.target.value)
-          }} />
-          <Employee name='Caleb' role='teacher' />
-          <Employee name='Abby' role='designer' />
-          <Employee name='John' role={role} />
+          <input
+            type='text'
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <div className='flex flex-wrap justify-center'>
+            <Employee
+              name='Caleb'
+              role='teacher'
+              img='https://images.pexels.com/photos/392018/pexels-photo-392018.jpeg'
+            />
+            <Employee
+              name='Abby'
+              role='designer'
+              img='https://images.pexels.com/photos/392018/pexels-photo-392018.jpeg'
+            />
+            <Employee
+              name='John'
+              role={role}
+              img='https://images.pexels.com/photos/392018/pexels-photo-392018.jpeg'
+            />
+            <Employee
+              name='Caleb'
+              role='teacher'
+              img='https://images.pexels.com/photos/392018/pexels-photo-392018.jpeg'
+            />
+            <Employee
+              name='Abby'
+              role='designer'
+              img='https://images.pexels.com/photos/392018/pexels-photo-392018.jpeg'
+            />
+            <Employee
+              name='John'
+              role={role}
+              img='https://images.pexels.com/photos/392018/pexels-photo-392018.jpeg'
+            />
+          </div>
         </>
       ) : (
-          'You are not allowed to see the employees'
+        'You are not allowed to see the employees'
       )}
     </div>
-  )
+  );
 }
 
 export default App
